b41048847ebfdbe3808a1971c4c99db37fe952ae
Hunzla Shafiq <157482220+HunzlaShafiq@users.noreply.github.com>
2025-07-23 19:07:19 +0500

    modify

--- lib/Screens/LogInScreen.dart
+++ lib/Screens/LogInScreen.dart
@@ -4,10 +4,7 @@ import 'package:classroom/Utils/Components/my_button.dart';
 import 'package:classroom/Utils/Components/my_text_field.dart';
 import 'package:classroom/Utils/Components/password_text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'HomeScreen.dart';
 import 'SignUpScreen.dart';
 import 'package:provider/provider.dart';
 
